d583e4dcbc23c86e2f3462c55b5342cc71caaf95
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-06-01 23:32:15 +0300

    display goals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -41,6 +41,18 @@ function Dashboard() {
       </section>
 
       <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   )
 }
